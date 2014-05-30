486d318d5864b3cd16e6f3e211937ef9ad0a2be8
jarland <me@jarland.me>
2014-05-30 14:43:42 -0500

    Create vzup.sh

---
+++ vzup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Container ID: "
+read ctid
+
+echo "OS: "
+read os
+
+echo "Hostname: "
+read hn
+
+echo "IP Address: "
+read ip
+
+echo "Memory: "
+read mem
+
+echo "Disk Size: "
+read disk
+
+vzctl create $ctid --ostemplate $os
+vzctl set $ctid --ipadd $ip --physpages $mem:$mem --diskspace $disk:$disk --nameserver 2001:470:20::2 --nameserver 8.8.8.8 --hostname $hn --save
+vzctl start $ctid
