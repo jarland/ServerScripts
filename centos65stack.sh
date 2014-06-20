be2549d3f54eb0719c3020e06fe197ac5cd041c3
jarland <me@jarland.me>
2014-06-19 20:34:48 -0500

    Update centos65stack.sh

--- centos65stack.sh
+++ centos65stack.sh
@@ -53,19 +53,38 @@ read DOMAIN;
 
 yum -y update
 rpm -ivh http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm
-yum -y install httpd mysql-server php php-pear php-xml php-mysql httpd-itk at wget php-gd php-mbstring
+yum -y install httpd mysql-server php php-pear php-xml php-mysql httpd-itk at wget php-gd php-mbstring nano expect varnish
 rpm -ivh https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_x86_64.rpm
+
+sed -i 's/VARNISH_LISTEN_PORT=6081/VARNISH_LISTEN_PORT=80/g' /etc/sysconfig/varnish
+sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
+
 service httpd restart
 service mysqld start
-mysql -e "DROP DATABASE test;"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQLPASS');"
-mysql -e "SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('$SQLPASS');"
-mysql -e "SET PASSWORD FOR 'root'@'::1' = PASSWORD('$SQLPASS');"
-mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root password "$SQLPASS"
+
+## Secure MySQL
+############
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \”$SQLPASS\r\”
+expect \"Change the root password?\"
+send \”n\r\”
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+echo "$SECURE_MYSQL"
+############
+
 sed -i 's/error\_reporting\ \=/#error\_reporting\ \=/g' /etc/php.ini
 echo "error_reporting = E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR" >> /etc/php.ini
 echo "error_log = /var/log/php.log" >> /etc/php.ini
@@ -80,8 +99,8 @@ echo "$PASS" | passwd "$USER" --stdin
 mkdir -p /home/$USER/$DOMAIN/{public_html,logs}
 chown -R $USER. /home/$USER
 touch /etc/httpd/conf.d/vhosts.conf
-echo "NameVirtualHost *:80" >> /etc/httpd/conf.d/vhosts.conf
-echo "<VirtualHost *:80>" >> /etc/httpd/conf.d/vhosts.conf
+echo "NameVirtualHost *:8080” >> /etc/httpd/conf.d/vhosts.conf
+echo "<VirtualHost *:8080>" >> /etc/httpd/conf.d/vhosts.conf
 echo "ServerName $DOMAIN" >> /etc/httpd/conf.d/vhosts.conf
 echo "ServerAlias www.$DOMAIN" >> /etc/httpd/conf.d/vhosts.conf
 echo "DocumentRoot /home/$USER/$DOMAIN/public_html/" >> /etc/httpd/conf.d/vhosts.conf
@@ -93,8 +112,10 @@ sed -i 's/#HTTPD/HTTPD/g' /etc/sysconfig/httpd
 sed -i 's/.worker/.itk/g' /etc/sysconfig/httpd
 chkconfig httpd on
 chkconfig mysqld on
+chkconfig varnish on
 service httpd restart
 service mysqld restart
+service varnish start
 clear
 echo "Installation complete. The content for $DOMAIN will be hosted in"
 echo "/home/$USER/$DOMAIN/public_html."
